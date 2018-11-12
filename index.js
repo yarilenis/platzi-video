c6156d340c921c4b5f0a038138b529e52bd44e9a
Yarilenis Laverde <yarilaverde@gmail.com>
2018-11-12 18:33:15 -0500

    inicial

---
+++ index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Media from './src/playlist/components/media';
+
+const app = document.getElementById('app')
+const holaMundo = <h1> Hola Muendo </h1>;
+ReactDOM.render(<Media />,app);
---
+++ src/playlist/components/media.js
@@ -0,0 +1,22 @@
+import React form 'react';
+
+class Media extends  React.Component{
+  render(){
+    return(
+      <div>
+        <div>
+          <img
+            src=""
+            alt=""
+            width={260}
+            height={160}
+            />
+          <h3>¿Por qué aprender React? </h3>
+          <p> Leonidas Esteban </p>
+        </div>
+      </div>
+    )
+  }
+}
+
+export defoudt Media;
